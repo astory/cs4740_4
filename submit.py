e809279d8dd7c46ee60a7b2ac3ab12e9edaf8aa5
Thomas Levine <thomas.levine@gmail.com>
2011-04-22 16:19:09 -0400

    add time stamp for submission

--- submit.py
+++ submit.py
@@ -1,20 +1,24 @@
 #!/bin/bash
 import urllib2
 import MultipartPostHandler
+import datetime
 
 def submit(netid,answers):
+	answersFile='tmp-answers.txt'
+	answersHandle=open(answersFile,'w')
+	answersHandle.write(answers)
+	answersHandle.close()
 	params= { 'netid':netid,
-                  'predictions': open('README.md','rb')
+                  'predictions': open(answersFile,'rb')
 		}
-	#params = {'user':'foo','file':open('foo.jpg', 'rb')}
 	opener = urllib2.build_opener(MultipartPostHandler.MultipartPostHandler)
 	urllib2.install_opener(opener)
 	req = urllib2.Request('http://www.cs.cornell.edu/w8/~luwang/cs4740_QA/getFile',params)
 	response = urllib2.urlopen(req).read().strip()
-	return response 
+	return response+'\n(Submitted at '+datetime.datetime.now().ctime()+')'
 
 def main():
-	print submit('tkl22','README.md')
+	print submit('tkl22','201 top_docs.201 Hawaii')
 
 if __name__ == "__main__":
     main()
