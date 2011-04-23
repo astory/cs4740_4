60e5fa7979cf8ce1c154de6bf897f684308d6c85
Thomas Levine <thomas.levine@gmail.com>
2011-04-23 12:08:12 -0400

    generalized fit

--- combine.py
+++ combine.py
@@ -5,7 +5,7 @@
 import numpy as np
 import mlpy
 
-def train():
+def train(model=mlpy.Svm):
 	'''
 	Train a model for classifying answers as correct or not
 	based on the confidence measures from various QA methods.
@@ -28,21 +28,24 @@ def train():
 	]
 
 	#Whether each of these answerse is correct
-	correct=[True,False,False,True,False,False,False,True,False,True,False]
-	#Confidence measures from each of the three answer extraction methods
-	confidence1=[0.378183359280229,0.275786651414819,0.48928187857382,0.154237459995784,0.196210152702406,
-	0.376024881959893,0.230388485593721,0.108338781050406,0.160494192386977,0.225011750124395,0.355593698564917]
-	confidence2=[0.001228122622706,0.0764752259710804,0.328412672388367,0.477082112804055,0.0747434163931757,
-	0.405251491814852,0.320799129549414,0.12499387213029,0.0304218637757003,0.0601054372964427,0.460696235764772]
-	confidence3=[0.64096049607421,0.557334918684016,0.315573251030097,0.376614285632968,0.601862544659525,
-	0.389133446694662,0.245313317359736,0.278548790918042,0.0664477733274301,0.588468289934099,0.476068765856326]
+	#correct=[True,False,False,True,False,False,False,True,False,True,False]
+	correct=[1,-1,-1,1,-1,-1,-1]
+	#Confidence measures from each of the three answer extraction methods for seven questions
+	confidence1=[0.978183359280229,0.275786651414819,0.48928187857382]
+	confidence2=[0.154237459995784,0.196210152702406,0.376024881959893]
+	confidence3=[0.230388485593721,0.108338781050406,0.160494192386977]
+	confidence4=[0.001228122622706,0.0764752259710804,0.328412672388367]
+	confidence5=[0.64096049607421,0.557334918684016,0.315573251030097]
+	confidence6=[0.389133446694662,0.245313317359736,0.278548790918042]
+	confidence7=[0.0664477733274301,0.588468289934099,0.476068765856326]
 
 	#Format them for the classification model https://mlpy.fbk.eu/data/doc/classification.html
-	xtr = np.array([confidence1,confidence2,confidence3,]
+	xtr = np.array([confidence1,confidence2,confidence3,confidence4,confidence5,confidence6,confidence7])
 	ytr = np.array(correct)
-	svm = mlpy.Svm()  # initialize Svm class
-	svm.compute(xtr, ytr)  # compute SVM
-	return svm
+	fit = model()  # initialize Svm class
+	fit.compute(xtr, ytr)  # compute SVM
+	#print svm.weights(xtr,ytr) #Weights/coefficients
+	return fit
 
 def test(svm):
 	'''
@@ -53,14 +56,19 @@ def test(svm):
 	'''
 	#Confidence measures for one particular answer
 	#from all [let's say 3] QA methods
-	confidence=[0.5995270445322,0.219738627163072,0.0829813567300638]
+	confidence=[0.9995270445322,0.378183359280229,0.275786651414819]
 
 	xts = np.array(confidence) # test point
-	print svm.predict(xts)  # predict SVM model on test point
-	print svm.realpred # real-valued prediction
+	# predict SVM on test corpus answer
+	if svm.predict(xts)==-1:
+		return False
+	elif svm.predict(xts)==1:
+		return True
+	#print svm.realpred # real-valued prediction. I'm not sure what this is. I guess it's the predicted Y value?
+
 
 def main():
-	test(train())
+	print test(train())
 
 
 if __name__ == "__main__":
