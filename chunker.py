df31cfb5a7c74cef01cee842db44987c6ce2e663
Whitney Foster <wlf2@cornell.edu>
2011-04-28 16:35:38 -0400

    initial chunker2

--- chunker.py
+++ chunker.py
@@ -5,21 +5,22 @@
 
 import init
 
-def get_chunked_sentences(namedEntity,q_id):
+def get_chunked_sentences(namedEntity,q_ids):
     chunk= []
-    topdoc = init.get_corpus(q_id)
-    doc_nums = topdoc.keys()
-    for key in doc_nums[:5]:
-        doc_text = topdoc[key].split()
-        # pull out sentences from docs that have that word
-        positions = [i for i,x in enumerate(doc_text) if x == namedEntity]
-        # get a random position
-        for pos in positions:
-            chunk.append(' '.join(doc_text[(pos - 5):(pos + 5)]))
+    for q_id in q_ids:
+        topdoc = init.get_corpus(q_id)
+        doc_nums = topdoc.keys()
+        for key in doc_nums[:5]:
+            doc_text = topdoc[key].split()
+            # pull out sentences from docs that have that word
+            positions = [i for i,x in enumerate(doc_text) if x == namedEntity]
+            # get a random position
+            for pos in positions:
+                chunk.append(' '.join(doc_text[(pos - 5):(pos + 5)]))
     return chunk
 
 if __name__=="__main__":
-    items= get_chunked_sentences('military',201)
+    items= get_chunked_sentences('military',[201,202,203])
     for item in items:
         print item
 
