a7c5eb5dc8fc4f10fdc8c420d7d37c58e50275d4
Thomas Levine <thomas.levine@gmail.com>
2011-05-06 14:38:14 -0400

    prediction data

--- one.py
+++ one.py
@@ -2,21 +2,31 @@
 from combine import *
 import chunker
 import mlpy
+import check_answers
 
 def question_candidates(q_id):
 #Incomplete
 	'''Select some useful subset of the candidates for a particular question.
 	Return them in a list.
 	'''
-	return [ ('400 micrograms', 'AP881126-0094', 55, 'VP'), ('farther', 'AP881126-0094', 56, 'NP'), ('away', 'AP881126-0094', 57, 'S')]
+	return [ ('sling', 'AP881126-0094', 55, 'VP'), ('farther', 'AP881126-0094', 56, 'NP'), ('away', 'AP881126-0094', 57, 'S')]
 
-def question_learning_data(first=201,last=399):
+def question_learning_data(first=204,last=204):
 	x=[]
 	y=[]
 	for q_id in range(first,last+1):
 		cand=question_candidates(q_id)
 		x=x+run_evaluators(cand)
 		y=y+map(lambda a:check_answers.check_answer(q_id,a),cand)
+	return y,x
+
+def question_prediction_data(q_id=202,candidate=('400 micrograms', 'AP881126-0094', 57, 'S')):
+	x=run_evaluators([candidate])
+	return x[0],q_id,candidate
+
+def main():
+	print question_learning_data()
+	print question_prediction_data()
 	
 if __name__ == '__main__':
 	main()
