db469a4f722dd4744e3b2b411a4402e4f575e578
Thomas Levine <thomas.levine@gmail.com>
2011-05-06 20:43:47 -0400

    classify questions by type (initial ngram)

--- question_type.py
+++ question_type.py
@@ -5,7 +5,7 @@ import read_questions
 def firstGram(sentence,n):
   return ' '.join(nltk.tokenize.word_tokenize(sentence)[:n])
 
-def get_question_types(n=2): #Unigrams by default
+def get_question_types(n=1): #Unigrams by default
   questions=read_questions.read_questions_no_answers()
   types=[]
   for question in questions:
@@ -13,6 +13,14 @@ def get_question_types(n=2): #Unigrams by default
     if a not in types:
       types.append(a)	
   return types
+
+def classify_questions(n=1):
+	questions={}
+	for t in get_question_types(n):
+		questions[t]=[]
+	for question in read_questions.read_questions_no_answers()
+		questions[firstGram(question[1])].append(question[0])
+	return questions
 
 def question_type(questions,n=2): #Unigrams by default
   for question in questions:
